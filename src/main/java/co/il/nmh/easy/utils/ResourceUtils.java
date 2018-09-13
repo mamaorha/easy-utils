813c2a4689d8c674a459b35e0014af94b0730853
Maor Hamami <mamaorha@gmail.com>
2018-09-13 17:07:09 +0300

    adjust resource utils

--- src/main/java/co/il/nmh/easy/utils/ResourceUtils.java
+++ src/main/java/co/il/nmh/easy/utils/ResourceUtils.java
@@ -21,15 +21,48 @@ public class ResourceUtils
 
 	public static URL getURL(String name)
 	{
-		return ClassLoader.getSystemClassLoader().getResource(name);
+		return getDefaultClassLoader().getResource(name);
 	}
 
 	public static String readResource(String fileName)
 	{
-		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-
-		InputStream resourceAsStream = classLoader.getResourceAsStream(fileName);
-
+		InputStream resourceAsStream = getDefaultClassLoader().getResourceAsStream(fileName);
 		return EasyUtils.readInputStream(resourceAsStream);
+	}
+
+	// this code was taken from org.springframework.util.ClassUtils
+	public static ClassLoader getDefaultClassLoader()
+	{
+		ClassLoader cl = null;
+
+		try
+		{
+			cl = Thread.currentThread().getContextClassLoader();
+		}
+
+		catch (Throwable ex)
+		{
+			// Cannot access thread context ClassLoader - falling back...
+		}
+
+		if (cl == null)
+		{
+			// No thread context class loader -> use class loader of this class.
+			cl = ResourceUtils.class.getClassLoader();
+
+			if (cl == null)
+			{
+				// getClassLoader() returning null indicates the bootstrap ClassLoader
+				try
+				{
+					cl = ClassLoader.getSystemClassLoader();
+				}
+				catch (Throwable ex)
+				{
+					// Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+				}
+			}
+		}
+		return cl;
 	}
 }
