e78fde9cdb5b0c0197169399d3556d4a9972b393
Maor Hamami <mamaorha@gmail.com>
2018-10-22 09:42:59 +0300

    adding 'allowAllSSL to easyRestClient

--- src/main/java/co/il/nmh/easy/utils/rest/EasyRestClient.java
+++ src/main/java/co/il/nmh/easy/utils/rest/EasyRestClient.java
@@ -5,9 +5,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import co.il.nmh.easy.utils.EasyInputStream;
 import co.il.nmh.easy.utils.exceptions.RestException;
@@ -20,6 +30,47 @@ import co.il.nmh.easy.utils.rest.data.RestClientResponse;
 
 public class EasyRestClient
 {
+	public static void allowAllSSL() throws KeyManagementException, NoSuchAlgorithmException
+	{
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager()
+		{
+			@Override
+			public java.security.cert.X509Certificate[] getAcceptedIssuers()
+			{
+				return null;
+			}
+
+			@Override
+			public void checkClientTrusted(X509Certificate[] certs, String authType)
+			{
+			}
+
+			@Override
+			public void checkServerTrusted(X509Certificate[] certs, String authType)
+			{
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier()
+		{
+			@Override
+			public boolean verify(String hostname, SSLSession session)
+			{
+				return true;
+			}
+		};
+
+		// Install the all-trusting host verifier
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+	}
+
 	public static RestClientResponse execute(String url, String method, EasyRestHeader headers) throws RestException
 	{
 		return executeRest(url, method, headers.getHeaders(), null);
